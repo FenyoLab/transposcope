4718e34061b03f6999bccef94cb93345eea1c6cb
Mark Grivainis <markgrivainis@gmail.com>
2019-01-09 16:32:14 -0500

    - Added setup.py - Added tox.ini - Added requirements.txt   + This lists the current versions of libraries used - All unit tests pass with tox - Corrected all of the flake8 linting errors

--- src/transposcope/parsers/main.py
+++ src/transposcope/parsers/main.py
@@ -6,8 +6,13 @@ def main():
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Parse TIPseqHunter or MELT output for visualization")
+    parser = argparse.ArgumentParser(
+        description="Parse TIPseqHunter or MELT output for visualization"
+    )
     # TODO read some other parsers help for pointers
     parser.add_argument('-s', '--source', help='The path to the source file')
-    parser.add_argument('-f', '--format', help='The input type [MELT, TIPseqHunter]')
+    parser.add_argument(
+        '-f', '--format',
+        help='The input type [MELT, TIPseqHunter]'
+    )
     args = parser.parse_args()

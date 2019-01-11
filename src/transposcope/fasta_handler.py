8579e9b0c8103d8cb0a670c868df2e2c126991c8
Mark Grivainis <markgrivainis@gmail.com>
2019-01-11 16:56:15 -0500

    + Added function to parse meta data from MELT vcf + Added extra linting tools    - Black    - Pylint    - Changed project to use virtualenv over conda + Cleaned up multiple linting issues

--- src/transposcope/fasta_handler.py
+++ src/transposcope/fasta_handler.py
@@ -6,41 +6,39 @@ from Bio.SeqIO.FastaIO import SimpleFastaParser
 
 class FastaHandler:
     def __init__(
-            self,
-            line1_reference_path,
-            reference_genome_folder_path,
-            line1_start,
-            line1_end
+        self,
+        line1_reference_path,
+        reference_genome_folder_path,
+        line1_start,
+        line1_end,
     ):
         self.LINE1_START = int(line1_start)
         self.LINE1_END = int(line1_end)
         self._reference_genome_seq = None
         self._reference_genome_folder_path = reference_genome_folder_path
         self._current_chromosome = None
-        with open(line1_reference_path, 'rU') as handle:
-            self._LINE1_REFERENCE_SEQ = next(
-                SimpleFastaParser(handle)
-            )[1].upper()
+        with open(line1_reference_path, "rU") as handle:
+            self._LINE1_REFERENCE_SEQ = next(SimpleFastaParser(handle))[
+                1
+            ].upper()
 
     def set_reference_genome(self, reference_genome_chromosome):
         logging.info(
-            'changing reference genome to {}'.format(
+            "changing reference genome to {}".format(
                 reference_genome_chromosome
             )
         )
         file_path = os.path.join(
             self._reference_genome_folder_path,
-            reference_genome_chromosome + '.fa'
+            reference_genome_chromosome + ".fa",
         )
-        with open(file_path, 'r') as handle:
+        with open(file_path, "r") as handle:
             parser = SimpleFastaParser(handle)
             self._reference_genome_seq = next(parser)[1].upper()
 
-    def get_reference_nucleotides_in_range(self,
-                                           start=None,
-                                           end=None,
-                                           chromosome=None
-                                           ):
+    def get_reference_nucleotides_in_range(
+        self, start=None, end=None, chromosome=None
+    ):
         if start < 0 or end < 0:
             raise ValueError("start and end positions must be positive")
         if chromosome != self._current_chromosome:
@@ -58,18 +56,14 @@ class FastaHandler:
 
     def generate_fasta_sequence(self, insertion):
         reference_genome_sequence = self.get_reference_nucleotides_in_range(
-            insertion.START,
-            insertion.END,
-            insertion.CHROMOSOME
+            insertion.START, insertion.END, insertion.CHROMOSOME
         )
         line_1_sequence = self.get_line1_nucleotides_in_range(
-            self.LINE1_START,
-            self.LINE1_END
+            self.LINE1_START, self.LINE1_END
         )
         insertion.GENOME_SEQUENCE = reference_genome_sequence
         insertion.LINE1_SEQUENCE = self.get_line1_nucleotides_in_range(
-            self.LINE1_START,
-            self.LINE1_END
+            self.LINE1_START, self.LINE1_END
         )
         if insertion.COMPLEMENT:
             line_1_sequence = self.reverse_complement(line_1_sequence)
@@ -79,13 +73,11 @@ class FastaHandler:
 
     @staticmethod
     def reverse_complement(sequence):
-        complement_sequence = ''.join([
-            {
-                'A': 'T',
-                'C': 'G',
-                'G': 'C',
-                'T': 'A',
-                'N': 'N'
-            }[x] for x in sequence.upper()])
+        complement_sequence = "".join(
+            [
+                {"A": "T", "C": "G", "G": "C", "T": "A", "N": "N"}[x]
+                for x in sequence.upper()
+            ]
+        )
         reverse_complement_sequence = complement_sequence[::-1]
         return reverse_complement_sequence
--- src/transposcope/parsers/melt_parser.py
+++ src/transposcope/parsers/melt_parser.py
@@ -1,46 +1,94 @@
+"""
+File: melt_parser.py
+Author: Mark Grivainis
+Email: mark.grivainis@fenyolab.org
+Github: https://github.com/yourname
+Description: Parser which extracts relavant data from MELT vcf files and
+             outputs a tab delimited file usable by TranspoScope.
+"""
+
+from collections import namedtuple
 import os
 import re
 
 
-def load_vcf(file_path: str) -> str:
-    with open(file_path, 'r') as fh:
-        for line in fh.readlines():
+def load_vcf(file_path):
+    """Create a generator which serves up the file one line at a time.
+    @param file_path: Path to the file to be iterated over
+    @type  file_path: str
+    @return: Generator which provides the file one line at a time
+    @rtype : Generator
+    """
+    with open(file_path, "r") as file_handle:
+        for line in file_handle.readlines():
             yield line.strip()
 
 
-def parse_meta_info(melt_file_handler: str) -> dict:
+def parse_meta_info(melt_file_handler):
+    """Extract meta data and header from the top of a MELT vcf file.
+   Meta data rows start with ## and the header starts with a singe #.
+
+    @param param:  melt_file_handler
+    @type  param:  Generator which returns a line of the melt file at a time.
+
+    @return:  Description
+    @rtype :  Type
+
+    @raise e:  Description
+    """
     header = None
     meta_data = {}
     for line in melt_file_handler:
-        if not line.startswith('##'):
+        if not line.startswith("##"):
             header = line[1:].split()
             break
-        key, value = line[2:].split('=', 1)
+        key, value = line[2:].split("=", 1)
         if key not in meta_data:
             meta_data[key] = {}
-        if value.startswith('<'):
-            data = re.search(r'ID=([^,]+)', value.strip('<>'))
+        if value.startswith("<"):
+            data = re.search(r"ID=([^,]+)", value.strip("<>"))
             current_id = data.group(1)
             meta_data[key][current_id] = {}
-            chrom_length = re.search(r'length=([^,]+)', value.strip('<>'))
+            chrom_length = re.search(r"length=([^,]+)", value.strip("<>"))
             if chrom_length:
-                meta_data[key][current_id]['length'] = chrom_length.group(1)
-            description = re.search(r'Description="([^"]+)', value.strip('<>'))
+                meta_data[key][current_id]["length"] = chrom_length.group(1)
+            description = re.search(r'Description="([^"]+)', value.strip("<>"))
             if description:
-                meta_data[key][current_id]['Description'] = description.group(1)
-            number = re.search(r'Number=([^,]+)', value.strip('<>'))
+                (
+                    meta_data[key][current_id]["Description"]
+                ) = description.group(1)
+                number = re.search(r"Number=([^,]+)", value.strip("<>"))
             if number:
-                meta_data[key][current_id]['Number'] = number.group(1)
-            data_type = re.search(r'Type=([^,]+)', value.strip('<>'))
+                meta_data[key][current_id]["Number"] = number.group(1)
+            data_type = re.search(r"Type=([^,]+)", value.strip("<>"))
             if data_type:
-                meta_data[key][current_id]['Type'] = data_type.group(1)
+                meta_data[key][current_id]["Type"] = data_type.group(1)
         else:
             meta_data[key] = value
 
-    return meta_data
+    return meta_data, header
 
 
-if __name__ == '__main__':
+def parse_vcf_content(melt_file_handler, header):
+    """return the contents of the melt vcf file as a list of loci
+
+    @param melt_file_handler: Generator which returns single lines of the vcf
+    file
+    @type  melt_file_handler: Generator -> str
+
+    @return: List of named tuples representing insertion sites
+    @rtype : List(NamedTuple)
+
+    @raise e:  None
+    """
+    insertion = namedtuple("Insertion", header)
+    contents = []
+    for line in melt_file_handler:
+        print(insertion(line.split()))
+    return contents
+
+
+if __name__ in "__main__":
     print(os.getcwd())
-    for row in load_vcf('./test/parsers/examples/melt.vcf'):
+    for row in load_vcf("./test/parsers/examples/melt.vcf"):
         print(row)
